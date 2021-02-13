1395d275703b7870a8151b1efd384952a134bf9d
Mager <andrew.mager@gmail.com>
2021-02-13 16:41:21 -0500

    Add image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import genImage from './gen-image.jpg'
 
 function App() {
   return (
     <div className="main">
       <h5>In loving memory</h5>
-      <h1>Genevieve Speier</h1>
+      <p className="image">
+        <img src={genImage} alt="Genevieve Speier" />
+      </p>
+      <h1>
+        <span>G</span>enevieve <span>S</span>peier
+      </h1>
       <p class="era">
         <em>May 2, 1948 – January 25, 2021</em>
       </p>
